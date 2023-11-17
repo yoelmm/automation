b9c99f030b933b582d3cbd0ba5cbae9c295fbedf
Yoel Malpica <joseyoelmalpica37@gmail.com>
2023-11-17 01:45:05 +0100

    Se configura la conexión con la BBDD MySQL y SQL SERVER

--- Reusable_functions/BBDD.py
+++ Reusable_functions/BBDD.py
@@ -1,30 +1,65 @@
 import mysql.connector
+import pyodbc
 
 class Gestion_BBDD:
-    def __init__(self):
 
-        # Configura la conexión a la base de datos
-        conexion = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Welcome123",
-            database="bdemployees"
-        )
+    #Método constructor de la clase
+    def __init__(self, entorno):
 
-        # Crea un objeto cursor para interactuar con la base de datos
-        cursor = conexion.cursor()
+        if entorno == 'TOOLS':
+            # Configura la conexión a la base de datos
+            conexion = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="Welcome123"
+            )
 
-        # Ejecuta una consulta
-        cursor.execute("SELECT * FROM departments")
+            # Crea un objeto cursor para interactuar con la base de datos
+            cursor = conexion.cursor()
 
-        # Obtiene los resultados
-        resultados = cursor.fetchall()
+            # Ejecuta una consulta
+            cursor.execute("SELECT * FROM bdemployees.departments")
 
-        # Itera sobre los resultados e imprímelos
-        for fila in resultados:
-            print(fila)
+            # Obtiene los resultados
+            resultados = cursor.fetchall()
 
-        # Cierra el cursor y la conexión
-        cursor.close()
-        conexion.close()
+            # Itera sobre los resultados e imprímelos
+            for fila in resultados:
+                print(fila)
+
+            # Cierra el cursor y la conexión
+            cursor.close()
+            conexion.close()
+
+        if entorno == 'BBDD_CLIENTE':
+            # Especifica los detalles de la conexión
+            driver = 'ODBC Driver 18 for SQL Server'
+            server = 'LAPTOP-DT5JG5J0'
+            database = 'GNF_ELEC'
+            username = 'sa'
+            password = 'Welcome123'
+
+            # Crea la cadena de conexión
+            connection_string = (
+                f'DRIVER={driver};'
+                f'SERVER={server};'
+                f'DATABASE={database};'
+                f'UID={username};'
+                f'PWD={password};'
+            )
+
+            # Intenta establecer la conexión
+            try:
+                connection = pyodbc.connect(connection_string)
+                print('Conexión exitosa')
+
+                # Aquí puedes realizar operaciones en la base de datos
+
+                # No olvides cerrar la conexión cuando hayas terminado
+                connection.close()
+
+            except pyodbc.Error as e:
+                print(f'Error en la conexión: {e}')
+
+
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # La variable especial __name__ en python permite determinar si el fichero de python que se ejecuta es el programa principal
 # El fichero que se establece como programa principal de ejecución asigna el siguiente valor a la variable especial __name__='__main__'
+
 from Reusable_functions import BBDD
+
 if __name__ == '__main__':
     print('Hola Mundo')
-    BBDD.Gestion_BBDD()
+    BBDD.Gestion_BBDD('BBDD_CLIENTE')
